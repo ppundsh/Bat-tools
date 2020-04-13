bbd439c8ec85f07dd55579255b27414293390926
harry <f789987f@gmail.com>
2020-04-13 17:03:51 +0800

    完成還原機制，添加WSL、預設開啟程式備份

--- Backup_Tool/Backup_Tool.bat
+++ Backup_Tool/Backup_Tool.bat
@@ -1,38 +1,47 @@
-@echo off && SETLOCAL ENABLEDELAYEDEXPANSION 
+@ECHO off && SETLOCAL ENABLEDELAYEDEXPANSION 
 :: 設定備份目標資料夾
 SET BackOutputDir=D:\Backup
 
 :: 設定備份項目路徑
 :: 名稱,路徑,是否排除catch
-SET Dir[1]=Intel,C:\Intel\
-
-goto test1
 SET Dir[1]=FF,%APPDATA%\Mozilla\Firefox\Profiles\
 SET Dir[2]=GC,%LOCALAPPDATA%\Google\Chrome\User Data\,1
 SET Dir[3]=Vivaldi,%LOCALAPPDATA%\Vivaldi\User Data\,1
 SET Dir[4]=Startup,%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\
 SET Dir[5]=SshKey,%HomePath%\.ssh\
 SET Dir[6]=RL2,%HomePath%\.rainlendar2\
-SET Dir[7]=Vscode,%HomePath%\.vscode
-SET Dir[8]=Intel,C:\Intel\
-:test1
+SET Dir[7]=Vscode1,%USERPROFILE%\.vscode\extensions\
+SET Dir[8]=Vscode2,%APPDATA%\Code\User\
+SET Dir[9]=Intel,C:\Intel\
+ 
+:: 7zip路徑
+SET UnzipPath=C:\Program Files\7-Zip
 
-set UnzipPath=C:\Program Files\7-Zip
+:: 備份wsl名稱
+SET WslName=kali-linux
 
-:: ~~~~選擇~~~
-if not defined BackOutputDir SET BackOutputDir=.
-if not exist "%UnzipPath%\7z.exe" echo 請下載並安裝7z並修改腳本內的路徑 https://www.developershome.com/7-zip/ && pause && exit
+GOTO GetAdmin
+:Start
+:: ~~~~~~~~~~~~~~~~~~~~~~~~ 選擇開始 ~~~~~~~~~~~~~~~~~~~~~~~
+TITLE 備份與還原自定路徑
+IF not defined BackOutputDir SET BackOutputDir=.
+IF not exist "%UnzipPath%\7z.exe" ECHO 請下載並安裝7z並修改腳本內的路徑 https://www.developershome.com/7-zip/ && PAUSE && EXIT
 
 
-echo 注：備份時產生的log檔為還原時必要檔案
-echo 1.備份
-echo 2.還原
-choice /c:12 /m:"選擇功能"
-if errorlevel 2 goto ChoiceData
-if errorlevel 1 goto StartBackup
-pause
+ECHO 注：備份時產生的log檔為還原時必要檔案
+ECHO 1.備份路徑
+ECHO 2.備份路徑、WSL、預設開啟程式
+ECHO 3.還原
+CHOICE /c:123 /m:"選擇功能"
+IF errorlevel 3 GOTO ChoiceDataR
+IF errorlevel 2 GOTO ChoiceBackup_All
+IF errorlevel 1 GOTO ChoiceBackup_Path
+
 :: ~~~~~~~~~~~~~~~~~~~~~~~~~~ 備份程序 ~~~~~~~~~~~~~~~~~~~~~~
-:StartBackup
+:ChoiceBackup_All
+SET DataB_All=1
+
+:ChoiceBackup_Path
 
 :: 定義日期
 FOR /F "tokens=1-3 delims=/ " %%a IN ("%date%") DO (SET _today=%%a%%b%%c)
@@ -45,39 +54,120 @@ SET A_SECS=%A:~6,2%
 
 SET StartTime=%_today%_%A_HOUR: =%%A_MINS: =%%A_SECS: =%
 
-title %StartTime% 備份
-mkdir "%BackOutputDir%\Backup"
-echo ;%StartTime% 備份開始>"%BackOutputDir%\Backup\%StartTime%.log"
+TITLE %StartTime% 備份
+IF not exist "%BackOutputDir%\Backup" MKDIR "%BackOutputDir%\Backup"
+ECHO ;%StartTime% 備份>"%BackOutputDir%\Backup\%StartTime%.log"
 
-
-SET Index=1
-set "unbackup="
+SET Index=0
+SET "unbackup="
 :BackupCalcStart
-if defined Dir[%Index%] (
-for /F "tokens=1,2,3 delims=," %%i in ("!Dir[%Index%]!") do (
+SET /A Index=%Index%+1
+IF defined Dir[%Index%] (
+    for /F "tokens=1,2,3 delims=," %%i IN ("!Dir[%Index%]!") do (
+        IF not exist %%j (
+            ECHO ;路徑不存在：%%i,%%j>>"%BackOutputDir%\Backup\%StartTime%.log"
+            GOTO BackupCalcStart
+        )
         SETLOCAL DISABLEDELAYEDEXPANSION
-        if defined %%k set unbackup=-xr!catch
+        IF defined %%k SET unbackup=-xr!catch
         "%UnzipPath%\7z.exe" a -tzip "%BackOutputDir%\Backup\%%i_%StartTime%.zip" "%%j*" %unbackup%
         SETLOCAL ENABLEDELAYEDEXPANSION
-        echo %%i,%%j>>"%BackOutputDir%\Backup\%StartTime%.log"
+        ECHO %%i,%%j>>"%BackOutputDir%\Backup\%StartTime%.log"
     )
-    SET /A Index=%Index%+1
     GOTO BackupCalcStart
 )
-pause
-exit
+
+ECHO.
+IF defined DataB_All (
+    ECHO 匯出WSL %WslName%
+    wsl --export %WslName% %BackOutputDir%\Backup\kali-linux_%StartTime%.tar
+    ECHO ;WSL,%WslName%,%BackOutputDir%\Backup\kali-linux_%StartTime%.tar>>"%BackOutputDir%\Backup\%StartTime%.log"
+    ECHO.
+    ECHO 匯出 DefaultAppAssociations
+    dism /online /Export-DefaultAppAssociations:%BackOutputDir%\Backup\DefaultApplicationAssociations_%StartTime%.xml
+    ECHO ;DAA,DefaultApplicationAssociations,%BackOutputDir%\Backup\DefaultApplicationAssociations_%StartTime%.tar>>"%BackOutputDir%\Backup\%StartTime%.log"
+
+)
+:BackupEnd
+ECHO.
+ECHO ~~~~~~~~~~~~~~~~~~~~~~~~~
+ECHO 備份結果：
+ECHO.
+TYPE "%BackOutputDir%\Backup\%StartTime%.log"
+ECHO ~~~~~~~~~~~~~~~~~~~~~~~~~
+ECHO.
+PAUSE
+EXIT
 
 
 :: ~~~~~~~~~~~~~~~~~~~~~~~~~~ 還原程序 ~~~~~~~~~~~~~~~~~~~~~~
-:ChoiceData
-cls
-echo 備份資料夾中的檔案列表：
-tree %BackOutputDir%\Backup\
-echo.
-set /p UserDate=請輸入要還原的日期(十二位數) :
-echo.
-if not exist %BackOutputDir%\Backup_%UserDate% echo 備份資料夾中無此日期，請重新選擇。&& pause && goto ChoiceData
-for /F "eol=; tokens=1,2 delims=," %%i in (%BackOutputDir%\Backup\%UserDate%.log) do (
+:ChoiceDataR
+TITLE 還原自定路徑
+CLS
+SET Index=0
+ECHO 備份資料夾中的檔案列表：
+FOR /F "tokens=1,2 delims=." %%i IN ('dir /b %BackOutputDir%\Backup\') do (
+    IF %%j == log (
+        SET /A Index=!Index!+1
+        SET Item[!Index!]=%%i
+        ECHO !Index!：%%i
+        )
+)
+FOR /L %%a IN (1 1 %Index%) DO (
+    SET choiceItem=!choiceItem!%%a
+)
+
+CHOICE /c:%choiceItem% /m:"選擇要還原的備份"
+FOR /L %%i IN (%Index%,-1,1) DO (
+    IF errorlevel %%i SET UserDate=!Item[%%i]! && GOTO StartR
+)
+:StartR
+SET UserDate=%UserDate: =%
+ECHO.
+ECHO ~~~~~~~~~~~~~~~~~~~~
+ECHO 即將還原 %UserDate% ，內容:
+ECHO.
+TYPE %BackOutputDir%\Backup\%UserDate%.log
+ECHO ~~~~~~~~~~~~~~~~~~~~
+ECHO.
+PAUSE
+ECHO.
+
+FOR /F "eol=; tokens=1,2 delims=," %%i IN (%BackOutputDir%\Backup\%UserDate%.log) do (
     "%UnzipPath%\7z.exe" x "%BackOutputDir%\Backup\%%i_%UserDate%.zip" -o"%%j" -y
     )
-pause
+
+FOR /F "eol=- tokens=1,2,3 delims=," %%i IN (%BackOutputDir%\Backup\%UserDate%.log) do (
+    IF "%%i" == ";WSL" (
+        wsl --import %%j c:\WSL %%k
+    )
+    IF "%%i" == ";DAA" (
+        dism /online /Import-DefaultAppAssociations:%%k
+    )
+    ) 
+PAUSE
+EXIT
+
+
+
+:GetAdmin
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+:: Your codes should start from the following line
+GOTO Start
